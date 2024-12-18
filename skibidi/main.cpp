7cba5a2cf615431cc8f3c7b058b42706752016bd
91tleg <maxchou0417@gmail.com>
2024-12-17 16:30:19 -0800

    Initial Commit

---
+++ skibidi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  skibidi
+//
+//  Created by Max Chou on 12/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

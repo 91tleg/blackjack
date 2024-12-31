76437c713d0d1916e99a639b15b9d6ae91e32d96
91tleg <maxchou0417@gmail.com>
2024-12-31 11:24:54 -0800

    Initial commit

--- skibidi/main.cpp
+++ skibidi/main.cpp
@@ -1,14 +1,18 @@
 //
 //  main.cpp
-//  skibidi
 //
 //  Created by Max Chou on 12/17/24.
 //
 
 #include <iostream>
+#include <string>
+#include "Deck.h"
+#include "Player.h"
+#include "BlackJackGame.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    BlackJackGame game;
+    game.play();
     return 0;
 }
